224b78c2cff054cb9083ec2aa69a4db5219f6e25
QA-Shivam <shivamkumar16399@gmail.com>
2025-07-30 08:39:17 +0530

    config-fix-githubaction

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
+# Load from config
 source config/config.properties
 
+# Merge inputs and config
 BROWSER="${INPUT_BROWSER:-$browser}"
 XMLFILE="${INPUT_XMLFILE:-$xmlfile}"
 RUNMODE="${INPUT_RUNMODE:-$runmode}"
 PLATFORM="${INPUT_PLATFORM:-$platform}"
 
+# Export to GitHub env
 echo "BROWSER=$BROWSER" >> $GITHUB_ENV
 echo "XMLFILE=$XMLFILE" >> $GITHUB_ENV
 echo "RUNMODE=$RUNMODE" >> $GITHUB_ENV
